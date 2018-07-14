312116c7a2491bbc44ff6989338ebf8c29f400da
naren g <naren.g@capillarytech.com>
2018-07-14 08:18:55 +0530

    changed server port

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/showDetails', function(req, res) {
             });
 });
 
-var server = app.listen(8080 || 5000, function () {
+var server = app.listen(3000, function () {
   var port = server.address().port;
   console.log("Express is working on port " + port);
 });
